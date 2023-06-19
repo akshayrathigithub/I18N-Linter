d735888229d537b1d1ace5a90a3d0a187918d546
Akshay Rathi <akshay.rathi@razorpay.com>
2023-06-19 12:56:56 +0530

    testing linter

--- files/tstThree.js
+++ files/tstThree.js
@@ -1,61 +1,26 @@
-import React, { useState } from 'react';
 
-const CurrencyConverter = () => {
-  const [amount, setAmount] = useState(100);
-  const [currency, setCurrency] = useState('USD');
-  const [convertedAmount, setConvertedAmount] = useState('');
+const CURRENCY_SYMBOL = {
+  INR: "₹",
+  MYR: "RM",
+  USD: "$"
+}
 
-  const handleAmountChange = (event) => {
-    setAmount(event.target.value);
-  };
-
-  const handleCurrencyChange = (event) => {
-    setCurrency(event.target.value);
-  };
-
-  const convertCurrency = () => {
-    let convertedValue;
-
-    if (currency === 'RM') {
-      convertedValue = `${amount} MYR`;
-    } else if (currency === 'INR') {
-      convertedValue = `${amount} ₹`;
-    } else if (currency === 'USD') {
-      convertedValue = `$${amount}`;
-    } else {
-      convertedValue = 'Currency not supported';
+function convertCurrency(amount, currency) {
+    // Calculate INR to USD conversion
+    const conversionRate = 0.014;
+    const convertedAmount = amount * conversionRate; // Conversion to USD
+    const INRsymbole = "₹";
+  
+    if (currency === "INR") {
+      return convertedAmount;
+    } else if (currency === "₹") {
+      return convertedAmount;
+    } else if (currency === "MYR") {
+      return convertedAmount * 4.55; // Conversion to MYR
+    }else if (currency === "USD") {
+      return convertedAmount * 80.98; // Conversion to USD
     }
-
-    setConvertedAmount(convertedValue);
-  };
-
-  return (
-    <div>
-      <h1>Currency Converter</h1>
-      <div>
-        <label htmlFor="amount">Amount:</label>
-        <input
-          type="number"
-          id="amount"
-          value={amount}
-          onChange={handleAmountChange}
-        />
-      </div>
-      <div>
-        <label htmlFor="currency">Currency:</label>
-        <select id="currency" value={currency} onChange={handleCurrencyChange}>
-          <option value="RM">Hello</option>
-          <option value="INR">Hello</option>
-          <option value="USD">Hello</option>
-          <option>RBI</option>
-          <option>GST</option>
-          <option>CIN</option>
-        </select>
-      </div>
-      <button onClick={convertCurrency}>Convert</button>
-      <p>Converted Amount: {convertedAmount}</p>
-    </div>
-  );
-};
-
-export default CurrencyConverter;
+     else {
+      return "Unsupported currency";
+    }
+  }
