9bb7fa2f6436b11f69213021dab141f3b066bc25
Akshay Rathi <akshay.rathi@razorpay.com>
2023-09-05 01:04:50 +0530

    testing

---
+++ component.test.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import { currencies } from "utils-file";
+import INR from "currency-utils";
+
+export default function Linter(){
+  const indianCurrency = currencies.INR;
+  const INRSymbol = indianCurrency.symbol;
+
+  if(indianCurrency === "INR"){
+    return(<div>
+      This is a Rupees testing account
+    </div>)
+  }
+
+  if(indianCurrency === "MYR"){
+    return (<div>This is a RBI testing account</div>)
+  }
+  return(<div></div>)
+}
